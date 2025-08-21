dfad459c5226825c070d578ac771de501c1948ce
khalifmv <mitra001@Mac-mini-Mitra001.local>
2025-08-21 13:55:06 +0700

    remove recursive updates

--- src/components/LocationHistory.vue
+++ src/components/LocationHistory.vue
@@ -2,16 +2,17 @@
 import { useUserInfoStore } from '../stores/User';
 import Card from './ui/Card.vue';
 import Title from './ui/Title.vue';
-
+import { computed } from "vue"
 
 const userInfo = useUserInfoStore()
+const locationHistory = computed(() => [...userInfo.locationHistory].reverse())
 </script>
 
 <template>
   <Card class="w-full flex flex-col bg-gray-200 border border-white h-full max-h-[650px] overflow-y-auto">
     <Title :size="'sm'">Riwayat lokasi</Title>
     <div class="mt-4">
-      <div v-for="value in userInfo.locationHistory.reverse()" :key="value.location.latitide" class="p-2 rounded-lg bg-white mb-2 flex flex-col gap-1">
+      <div v-for="value in locationHistory" :key="value.location.latitide" class="p-2 rounded-lg bg-white mb-2 flex flex-col gap-1">
         <div class="flex justify-between items-center">
           <div class="font-bold"><Title :size="'md'">{{ value.location.city }}</Title></div>
           <!-- <Title :size="'md'">{{ value.location.subdistrict }}</Title> -->
